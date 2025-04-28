c3c9611e03cde32658adaecd1f196a41f80af1d4
Koyle1 <coyfelix7@gmail.com>
2025-04-28 09:15:01 +0200

    added some code for data fetching

---
+++ data_fetcher.py
@@ -0,0 +1,14 @@
+import cellxgene_census
+
+class data_fetcher():
+
+    def __init__(self):
+        pass
+
+    def fetch_data(self):
+        '''
+            Input:
+            Output: dataframe
+            Description: Function fetches Data from the cellxgene database
+        '''
+        pass
